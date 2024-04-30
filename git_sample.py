59e2d944d8c8fcdc1ca7b3e42dba6ed43558b8bb
taroon23 <tganesh@usc.edu>
2024-04-29 19:55:21 -0700

    change 1

--- git_sample.py
+++ git_sample.py
@@ -1,36 +1,18 @@
 import pandas as pd
-import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import matplotlib.pyplot as plt
-import seaborn as sns
+import streamlit as st
 
+# Read the CSV files
+amazon_data = pd.read_csv('../amazon_data.csv', names=['Brand', 'Date', 'Review'])
+google_data = pd.read_csv('../google_data.csv', names=['Brand', 'Date', 'Review'])
 
-amazon_data_file_path = '../amazon_data.csv'
-
-amazon_headers = ['Brand', 'Date', 'Review']
-
-# Read the CSV file into a pandas DataFrame with headers
-amazon_data = pd.read_csv(amazon_data_file_path, names=amazon_headers)
-
-
-
-google_data_file_path = '../google_data.csv'
-
-google_headers = ['Brand', 'Date', 'Review']
-
-# Read the CSV file into a pandas DataFrame with headers
-google_data = pd.read_csv(google_data_file_path, names=google_headers)
-
-
+# Read stock data
 tickers = ['ADDYY', 'NKE', 'SKX', 'UAA', 'PUM.DE']
 brand_mapping = {'ADDYY': 'Adidas', 'NKE': 'Nike', 'SKX': 'Skechers', 'UAA': 'Under Armour', 'PUM.DE': 'Puma'}
 stock_dfs = []
 
 for ticker in tickers:
-    #query_string = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1=1514764800&period2=1713404238&interval=1d&events=history&includeAdjustedClose=true"
-    
     query_string = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1=1464739200&period2=1714351190&interval=1d&events=history&includeAdjustedClose=true"
-    
     stock_df = pd.read_csv(query_string)
     stock_df['Ticker'] = ticker
     stock_df['Brand'] = brand_mapping[ticker]
@@ -38,16 +20,10 @@ for ticker in tickers:
 
 stock_data = pd.concat(stock_dfs, ignore_index=True)
 
-
+# Merge data
 amazon_google_data = pd.concat([amazon_data, google_data], ignore_index=True)
-
-# Merge the two DataFrames on 'Brand' and 'Date'
 combined_data = pd.merge(amazon_google_data, stock_data[['Date', 'Close', 'Brand']], on=['Date', 'Brand'], how='left')
-
-stock_analysis_data = combined_data = combined_data.dropna(subset=['Close'])
-
-
-
+stock_analysis_data = combined_data.dropna(subset=['Close'])
 
 # Initialize the VADER sentiment analyzer
 analyzer = SentimentIntensityAnalyzer()
@@ -68,4 +44,8 @@ stock_analysis_data['Review'] = stock_analysis_data['Review'].astype(str)
 # Apply sentiment analysis to each review
 stock_analysis_data['Sentiment'] = stock_analysis_data['Review'].apply(get_sentiment)
 
-print(stock_analysis_data)
+# Streamlit app
+st.title('Stock Sentiment Analysis')
+
+# Display data
+st.write(stock_analysis_data)
