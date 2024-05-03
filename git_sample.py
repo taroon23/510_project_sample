68eb9e7e5ed44a7e87df02037f9726d581548a91
taroon23 <tganesh@usc.edu>
2024-05-02 17:12:53 -0700

    Update git_sample.py

--- git_sample.py
+++ git_sample.py
@@ -322,7 +322,7 @@ def main():
             num_blank_lines = st.empty()
 
             # Display blank lines for vertical centering
-            for _ in range(2):
+            for _ in range(1):
                 st.write("")
 
             # Display average price and ratings
